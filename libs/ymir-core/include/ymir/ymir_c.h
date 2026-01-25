aa5234c86c5a3ac54f9647fd32216b9ee71d0686
Lennart Kotzur <len.kot@icloud.com>
2026-01-25 21:44:13 +0100

    feat/v1.1: added basic framebuffer view   - expanded result enum in ymir_c header   - added fb format and info   - copy_framebuffer function into callsite   - added initial comments, to be expanded   - reset now takes fb video out into account   - content view expanded with fb output   - MetalFramebufferView + shader for video out

--- libs/ymir-core/include/ymir/ymir_c.h
+++ libs/ymir-core/include/ymir/ymir_c.h
@@ -18,7 +18,8 @@ typedef enum ymir_result {
     YMIR_RESULT_INVALID_ARGUMENT = 1,
     YMIR_RESULT_IO_ERROR = 2,
     YMIR_RESULT_SIZE_MISMATCH = 3,
-    YMIR_RESULT_INTERNAL_ERROR = 4
+    YMIR_RESULT_INTERNAL_ERROR = 4,
+    YMIR_RESULT_NOT_READY = 5
 } ymir_result_t;
 
 typedef enum ymir_log_level {
@@ -45,11 +46,25 @@ YMIR_CORE_EXPORT void ymir_destroy(ymir_handle_t *handle);
 // Note: devlog output is global; the most recently set callback receives it.
 YMIR_CORE_EXPORT void ymir_set_log_callback(ymir_handle_t *handle, ymir_log_callback_t callback, void *user_data);
 
+typedef enum ymir_framebuffer_format {
+    YMIR_FRAMEBUFFER_FORMAT_XBGR8888 = 1
+} ymir_framebuffer_format_t;
+
+typedef struct ymir_framebuffer_info {
+    uint32_t width;
+    uint32_t height;
+    uint32_t stride_bytes;
+    ymir_framebuffer_format_t format;
+} ymir_framebuffer_info_t;
+
 YMIR_CORE_EXPORT ymir_result_t ymir_set_ipl_path(ymir_handle_t *handle, const char *path);
 YMIR_CORE_EXPORT ymir_result_t ymir_reset(ymir_handle_t *handle, bool hard_reset);
 
 YMIR_CORE_EXPORT uint64_t ymir_step_master_sh2(ymir_handle_t *handle);
 YMIR_CORE_EXPORT void ymir_run_frame(ymir_handle_t *handle);
+
+YMIR_CORE_EXPORT ymir_result_t ymir_copy_framebuffer(ymir_handle_t *handle, void *out_buffer, size_t buffer_size,
+                                                     ymir_framebuffer_info_t *out_info, uint64_t *out_frame_id);
 
 YMIR_CORE_EXPORT const char *ymir_get_last_error(ymir_handle_t *handle);
 YMIR_CORE_EXPORT const char *ymir_get_version_string(void);
