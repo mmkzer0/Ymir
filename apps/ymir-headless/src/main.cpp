808b49162d718ba589daeec01396b110fef30ba9
Lennart Kotzur <len.kot@icloud.com>
2026-05-08 14:56:31 +0200

    build(headless): add debug app skeleton targets

---
+++ apps/ymir-headless/src/main.cpp
@@ -0,0 +1,9 @@
+#include <fmt/format.h>
+
+int main(int argc, char **argv) {
+    (void)argc;
+    (void)argv;
+
+    fmt::println("ymir-headless: Ymir headless debug worker (stub)");
+    return 0;
+}
